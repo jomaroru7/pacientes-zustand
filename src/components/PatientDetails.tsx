d6d697a7f8da5a29261e1f9bebbd23bd582709f1
Chema <jomaroru7@gmail.com>
2024-12-17 09:32:59 +0100

    Added delete patient functionality

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,3 +1,4 @@
+import { usePatientStore } from "../store"
 import { Patient } from "../types"
 import PatientDetailItem from "./PatientDetailItem"
 
@@ -6,6 +7,8 @@ type PatientDetailsProps = {
 }
 
 export default function PatientDetails({ patient }: PatientDetailsProps) {
+
+  const deletePatient = usePatientStore((state) => state.deletePatient)
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
       <PatientDetailItem
@@ -38,7 +41,9 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
         >Editar</button>
         <button
+          type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => deletePatient(patient.id)}
         >Eliminar</button>
       </div>
     </div>
