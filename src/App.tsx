65c7f57eaad1dd589fadf2dd44dcae0450b86d93
Chema <jomaroru7@gmail.com>
2024-12-17 10:13:18 +0100

    Added pop-ups for events with react toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+
+import { ToastContainer } from "react-toastify"
 import PatientForm from "./components/PatientForm"
 import PatientList from "./components/PatientList"
+import "react-toastify/dist/ReactToastify.css"
 
 
 function App() {
@@ -16,6 +19,7 @@ function App() {
           <PatientList/>
         </div>
       </div>
+      <ToastContainer/>
     </>
   )
 }
