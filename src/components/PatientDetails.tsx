b06eb453fff46d2dd3b13091b8f35bee33c7a424
Chema <jomaroru7@gmail.com>
2024-12-17 10:01:28 +0100

    Completed edit patient functionality

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -37,7 +37,7 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
         data={patient.symptoms}
       />
 
-      <div className="flex justify-between mt-10">
+      <div className="flex flex-col lg:flex-row gap-3 justify-between mt-10">
         <button
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
           onClick={() => getPatientById(patient.id)}
